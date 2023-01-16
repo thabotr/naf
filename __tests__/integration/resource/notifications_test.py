fad71acdaa066abb77d6b4916d449d8d8f3d7c12
thabor <1831661@students.wits.ac.za>
2023-01-16 03:15:43 +0200

    none: GET /notifications int tests

--- __tests__/integration/resource/notifications_test.py
+++ __tests__/integration/resource/notifications_test.py
@@ -1,57 +1,69 @@
 from assertpy import assert_that
-from routes import TestCaseWithHTTP, Routes
+from routes import TestCaseWithHTTP, Routes, getAuthHeaders
 from http import client
 import json
 import requests
+from datetime import datetime
 
-class POSTNotifications(TestCaseWithHTTP):
+class GETNotifications(TestCaseWithHTTP):
   notificationsURL = f"{Routes.BASE_PATH}/notifications"
 
-  def postNotifications(self, data):
-    data_string = json.dumps(data)
-    self.conn.request('POST', self.notificationsURL, data_string, self.authedHeaders)
+  def getNotifications(self, headers: dict[str, str]):
+    self.conn.request('GET', self.notificationsURL, headers=self.authedHeaders | headers)
     response = self.conn.getresponse()
     status, body = response.status, response.read().decode("utf-8")
-    return status, body
+    return (status, body)
 
   def testBadRequestOnMissingMessagesSince(self):
-    """given a request body without the 'messagesSince' instruction it returns status 'Bad Request'"""
+    """given a request without the 'messagessince' header instruction it returns status 'Bad Request'"""
     missing_messages_since = {}
-    status, body = self.postNotifications(missing_messages_since)    
+    status, body = self.getNotifications(missing_messages_since)    
     assert_that(status).is_equal_to(client.BAD_REQUEST)
-    assert_that(body).is_equal_to("missing field 'messagesSince'")
+    assert_that(body).is_equal_to("missing header 'messagessince'")
   
   def testBadRequestOnMessagesSinceInvalidDateTime(self):
-    """given a request body with an invalid 'messagesSince' time stamp it returns status 'Bad Request'"""
+    """given a request with an invalid 'messagessince' header time stamp it returns status 'Bad Request'"""
     bad_messages_since_timestamp = {
-      'messagesSince': '2023/a1/1'
+      'messagessince': '2023/a1/1'
     }
-    status, body = self.postNotifications(bad_messages_since_timestamp)
+    status, body = self.getNotifications(bad_messages_since_timestamp)
     assert_that(status).is_equal_to(client.BAD_REQUEST)
-    assert_that(body).is_equal_to("instruction field 'messagesSince' should be format '%Y-%m%d %H:%M:%S'")
+    assert_that(body).is_equal_to("instruction header 'messagessince' should a UTC time of format '%Y-%m-%d %H:%M:%S'")
   
-  def _testNewMessageEventCodeOnNewMessage(self):
-    """
-    Manual test - when a user is sent a new message, the response stream changes from IDLE event
-    codes '0' to NEW_MESSAGE event codes '1'
-    
-    Produce behaviour:
-      1. Change 'messageSince' in request payload to latest date past which the user has no messages;
-      2. Run this test
-      3. Add a new message with 'to_user' = 1 in SQL;
-    and the streamed response here should the start producing different results.
-    
-    Ideally: we would send two requests, in parallel, from two different users - one for listening to
-    notifications and one for posting a new message and then assert that the listener does get the new
-    message event code when the post message is complete. However, we cannot do this since requests can 
-    only be executed sequentially from the same machine.
-    """
-    data = json.dumps({
-      'messagesSince': '2023/1/13'
+  def testIdleEventCodeOnNoNotifications(self):
+    """given a request with a valid 'messagessince' after which no new messages 
+    have been sent to the user then it returns 'OK' and IDLE event code in the body"""
+    before_message = {
+      'messagessince' : str(datetime.utcnow())
+    }
+    status, body = self.getNotifications(before_message)
+    assert_that(status).is_equal_to(client.OK)
+    idle_event_code = '0'
+    assert_that(body).is_equal_to(idle_event_code)
+
+  def sendMessageFromOtherUser(self):
+    message = json.dumps({
+      'text': 'Test.POSTNofitications.testNewMessageEventCodeOnNewMessage',
+      'toHandle': self.handle,
     })
-    remote = f"https://{Routes.PROD_SERVER}"
-    local = f"http://{Routes.HOST}:{Routes.PORT}"
-    url = (remote if Routes.RUN_AGAINST_PROD else local) + self.notificationsURL
-    response = requests.post( url, data, stream=True, headers=self.authedHeaders)
-    for data in response.iter_content(chunk_size=4096):
-      print(data[:1].decode())
+    auth = getAuthHeaders('w/testHandle2', 'testToken2')
+    self.conn.request('POST', f"{Routes.BASE_PATH}/messages", message, auth)
+    status = self.conn.getresponse().status
+    assert_that(status).is_equal_to(client.CREATED)
+
+  def testNewMessageEventCodeOnNewMessage(self):
+    """given a request with a valid 'messagessince' header it should return status 
+    'OK' and NEW_MESSAGE eventy code when the user has been sent a message after the
+    timestamp 'messagessince' 
+    """
+    since = {
+      'messagessince': str(datetime.utcnow())
+    }
+    self.sendMessageFromOtherUser()
+    status, body = self.getNotifications(since)
+    new_message_event = '1'
+    assert_that(status).is_equal_to(client.OK)
+    assert_that(body).is_equal_to(new_message_event)
+
+
+    
